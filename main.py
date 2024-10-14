264aa81384aad534e0982a8e8f60c285ca0d662b
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-10-14 13:46:29 +0200

    Add main.py file

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/status")
+def status() -> dict:
+    return {"status": "ok"}
